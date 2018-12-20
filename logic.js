2cf881aff5063e1b7bf6df434e33352585821f2b
Phyllis Summers <summerspj@live.com>
2018-12-20 15:20:52 -0700

    working on popup and layers

--- logic.js
+++ logic.js
@@ -5,7 +5,7 @@ var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.pn
   maxZoom: 18,
   id: "mapbox.streets",
   accessToken: API_KEY
-});
+})
 
 var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", 
 {
@@ -14,6 +14,64 @@ var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?
   id: "mapbox.streets-satellite",
   accessToken: API_KEY
 });
+
+// Initialize all of the LayerGroups we'll be using
+var layers = {
+    Street: new L.LayerGroup(),
+    Dark: new L.LayerGroup(),
+    Quakes: new L.LayerGroup(),
+    Faults: new L.LayerGroup()
+  };
+
+// Create the map with our layers
+var myMap = L.map("map", {
+    center: [6.4238, -10.5897],
+    zoom: 2.1,
+    minZoom: 2.1,
+    layers: [
+        streetmap,
+        layers.Quakes,
+        layers.Faults
+        ]
+    });
+
+// Add base tile layer to the map
+var baseMaps = {
+  "Street Map": streetmap,
+  "Satellite Map": darkmap
+};
+
+//Create an overlays object to add to the layer control
+var overlays = {
+  "Earthquakes": layers.Quakes,
+  "Faults": layers.Faults
+};
+
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function(map) 
+{
+var div = L.DomUtil.create('div', 'info legend'),
+    mags = [0, 1, 2, 3, 4, 5],
+    labels = []
+
+    div.innerHTML += '<b>Earthquakes/</b><br>'
+    div.innerHTML += '<b>Magnitude</b><br>' 
+    div.innerHTML += '<b>This Past Week</b><br><br>' 
+
+    // loop through our density intervals and generate a label with a colored 
+    //square for each interval
+    for (var i = 0; i < mags.length; i++) {
+    div.innerHTML +=
+        '<i style="background:' + getColor(mags[i] + 1) + '"></i> ' +
+        mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+legend.addTo(myMap);
+  
+// Create a control for our layers, add our overlay layers to it
+L.control.layers(baseMaps, overlays).addTo(myMap);
+
 
 // Create API endpoint
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -33,7 +91,7 @@ d3.json(url, function (data)
     {
       return new L.CircleMarker(latlng,
         {
-          radius: (feature.properties.mag * 4),
+          radius: (feature.properties.mag * 4),  
           fillOpacity: 1, 
           color: 'black', 
           fillColor: getColor(feature.properties.mag), 
@@ -49,28 +107,28 @@ d3.json(url, function (data)
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + 
         "</h3><hr><p>" + "Magnitude:  " + (feature.properties.mag) + "</p>"
       )
-    },
-
-  });
-
-  // get Fault data and add to Fault layer
-  d3.json(platesLink, function(data)
-  {
-    var faultFeatures = data.features
-    var styling = 
-    {
-        "fillOpacity": 0
     }
 
-    var faults = L.geoJSON(faultFeatures, 
-      {
-        style: function(feature)
-        {
-            return styling
-        }
-      })
-  createMap(earthquakes, faults)  
-  })
+  }).addTo(layers.Quakes);
+
+// // get Fault data and add to Fault layer
+//   d3.json(platesLink, function(data)
+//   {
+//     var faultFeatures = data.features
+//     var styling = 
+//     {
+//         "fillOpacity": 0
+//     }
+
+//     var faults = L.geoJSON(faultFeatures, 
+//       {
+//         style: function(feature)
+//         {
+//             return styling
+//         }
+//       }).addTo(myMap)
+//     //   createMap(earthquakes, faults)  
+//   }).addTo(layers.Faults)
 });
 
 // define function for get color
@@ -83,51 +141,3 @@ function getColor(d)
         d > 4 && d <= 5   ? 'pink' :
                     'red';
 } 
-
-function createMap(Quakes, Faults)
-{
-  // Add base tile layer to the map
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Satellite Map": darkmap
-  };
-
-  // Create an overlays object to add to the layer control
-  var overlays = {
-    "Earthquakes": Quakes,
-    "Faults": Faults
-  };
-
-  // Create the map with our layers
-  var myMap = L.map("map", {
-    center: [6.4238, -10.5897],
-    zoom: 2.1,
-    minZoom: 2.1,
-    layers: [streetmap, Quakes, Faults]
-  });
-
-  var legend = L.control({position: 'bottomright'});
-  legend.onAdd = function(map) 
-  {
-    var div = L.DomUtil.create('div', 'info legend'),
-      mags = [0, 1, 2, 3, 4, 5],
-      labels = []
-
-      div.innerHTML += '<b>Earthquakes/</b><br>'
-      div.innerHTML += '<b>Magnitude</b><br>' 
-      div.innerHTML += '<b>This Past Week</b><br><br>' 
-
-      // loop through our density intervals and generate a label with a colored 
-      //square for each interval
-      for (var i = 0; i < mags.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(mags[i] + 1) + '"></i> ' +
-            mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
-      }
-      return div;
-  };
-  legend.addTo(myMap);
-  
-  // Create a control for our layers, add our overlay layers to it
-  L.control.layers(baseMaps, overlays).addTo(myMap);
-};
